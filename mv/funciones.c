d790c7ef308c851121963065e5108811150f3ae7
Mauro Scardamaglia <mauroscardamaglia10@gmail.com>
2026-09-19 10:00:53 -0300

    terminé de implementar varias funciones auxiliares

--- mv/funciones.c
+++ mv/funciones.c
@@ -7,37 +7,50 @@
 
 // 1 función por mnemónico (28 mnémonicos + 5 sin definir (notDefined))
 
-// funciones auxiliares:
-int leerOperando(int descriptor){
-    int tipo = (descriptor >> 24) & 0xFF;
-    int datosOperando = descriptor & 0xFFFFFF;
+// el dato lo "casteo" a unsigned int para hacer el shift lógico
+char devolverByte(unsigned int dato, int nroByte){ // que nombre le ponemos a los parámetros
+    // el nro byte es el byte a devolver, siendo 0 el byte de menor significado, 1: el segundo de menor significado, ....
+    dato = dato >> 8 * nroByte;
+    dato &= 0xFF;
+}
+
+// falta configurar el mbr, y un par de cosas más
+
+int leerOperando(int operando, int cantBytes){
+    int tipo = (devolverByte(operando,3));
+    int datosOperando = operando & 0xFFFFFF;
 
     switch (tipo){
         case 1: // registro
             return registros[datosOperando & 0x1F];
 
         case 2: // inmediato
-            return (int)(short int)(datosOperando & 0xFFFF); // sign-extend 16->32 bits
+            return (datosOperando & 0xFFFF);
 
         case 3: // memoria
-            return leerDeMemoria(datosOperando);
-
+            calcularPunteroLogico(datosOperando);
+            calcularPunteroFisico(cantBytes);
+            leerDeMemoria();
         default: // ninguno
-            return 0;
+            return 0; // está bien esto??
     }
 }
 
-void escribirOperando(int descriptor, int valor){
-    int tipo = (descriptor >> 24) & 0xFF;
-    int datosOperando = descriptor & 0xFFFFFF;
-
+void escribirOperando(int operando, int valor, int cantBytes){
+    int tipo = devolverByte(operando,3);
+    int datosOperando = operando & 0xFFFFFF;
+    int aux;
     switch (tipo){
         case 1: // registro
             registros[datosOperando & 0x1F] = valor;
             break;
 
         case 3: // memoria
-            escribirEnMemoria(datosOperando, valor);
+            calcularPunteroLogico(datosOperando);
+            calcularPunteroFisico(cantBytes);
+            aux = leerOperando(valor,4);
+            escribirMBR(aux);
+            escribirEnMemoria();
             break;
 
         default: // ninguno o inmediato (no son destino válido)
@@ -45,18 +58,64 @@ void escribirOperando(int descriptor, int valor){
     }
 }
 
-void leerDeMemoria(int operandoMemoria){}
+void leerDeMemoria(){
+    int n; // cant bytes a leer
+    n = devolverByte(registros[5],3);
+    n = n << 8;
+    n |= devolverByte(registros[5],2);
 
-void escribirEnMemoria(int operandoMemoria, int valor){}
-
-int calcularPunteroLogico(int operandoMemoria){
-    // return puntLogico
+    int aux = 0; // por las dudas que el n sea 0, hay que verificarlo en el testing
+    for(int i=0; i < n; i++){
+        aux = aux << 8;
+        aux = aux | memoria[registros[5] + i];
+    }
+    escribirMBR(aux);
 }
 
-int calcularPunteroFisico(int puntLogico){
+
+void escribirEnMemoria(){
+    int mbr;
+    int n; // cant bytes a escribir
+    mbr = registros[6];
+    n = devolverByte(registros[5],3);
+    n = n << 8;
+    n |= devolverByte(registros[5],2);
+
+    int aux = 0; // por las dudas que el n sea 0, hay que verificarlo en el testing
+    for(int i=0; i < n; i++){
+        memoria[registros[5]+i] = devolverByte(mbr,n-1 -i);
+    }
+}
+
+void escribirMBR(int valor){ // configura el MBR
+    registros[6] = valor;
+}
+
+void calcularPunteroLogico(int operandoMemoria){ // configura el LAR
+    // lar -> reg[4]
+    int nroReg = devolverByte(operandoMemoria,0);
+    int puntLog = registros[nroReg];
+    int aux = devolverByte(operandoMemoria,2);
+    aux = aux << 1;
+    aux = aux | devolverByte(operandoMemoria,1);
+    puntLog += aux; // acá habría que verificar si no nos caímos del segmento   
+    registros[4] = puntLog; // reg[4] -> LAR
+}
+
+void calcularPunteroFisico(int cantidadDeBytes){ // configura el MAR
     //return puntFisico;
+    int puntLogico = registros[4]; // lar
+    int nroSegm = devolverByte(puntLogico,2);
+    int puntFisico = tablaSegm[nroSegm].base; // dirección base
+    int aux = devolverByte(puntLogico,1);
+    aux = aux << 1;
+    aux = aux | devolverByte(puntLogico,0);
+    puntFisico += aux; // + offset
+    // acá habría que verificar si no nos caímos del segmento
+    cantidadDeBytes = cantidadDeBytes << 16;
+    puntFisico |= cantidadDeBytes; // cargamos en la parte alta del MAR la cant de bytes a leer/escribir
+    registros[5] = puntFisico; // reg[5] -> MAR
 }
-
 
 
 //--------------    NO DEFINIDO  ---------------------
@@ -70,7 +129,7 @@ void notDefined(){
 
 void sys(){
     int operando = registros[2]; // OP1: único operando (código de llamada al sistema)
-    int numeroLlamada = leerOperando(operando);
+    int numeroLlamada = leerOperando(operando,4);
 
     switch (numeroLlamada){
             case 1: // READ
@@ -110,9 +169,12 @@ int overflow(){
 }
 
 void jmp(){
-    int offset = registros[2]; // la idea es obtener el valor a desplazar
-    // ahora esto está mal, el 2do operando tiene el valor a desplazar si es un inmediato, si es un registro o memoria hay que obtenerlo de una manera diferente
-    registros[0] = registros[27] + offset; // ip = ds + offset
+    int aux = leerOperando(registros[2],4); // leer op1, 4 bytes
+    aux = aux << 8;
+    aux |= 26; // registro CS
+    calcularPunteroLogico(aux);    
+    calcularPunteroFisico(4); // está bien esto?? no voy a leer bytes, solo calcular la posFisica
+    registros[0] = registros[5]; // IP = MAR
     // acá sería necesario hacer una validación para ver que no nos caemos del code segment
     // o en su defecto, llamar a una función que realice esta asignación y validación...
 }
